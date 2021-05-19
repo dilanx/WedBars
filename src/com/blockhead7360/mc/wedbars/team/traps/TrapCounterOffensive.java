0905f033761c8ca73fdd41ad8722662bb44dd06a
Jack Burkhardt <mail@jackburkhardt.com>
2021-05-19 15:11:18 -0500

    trap work

--- src/com/blockhead7360/mc/wedbars/team/traps/TrapCounterOffensive.java
+++ src/com/blockhead7360/mc/wedbars/team/traps/TrapCounterOffensive.java
@@ -10,14 +10,14 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-public class TrapBlindness extends TeamTrap {
+public class TrapCounterOffensive extends TeamTrap {
 
-    // Still called TrapBlindness but now has slow.
-    public ItemStack icon() {
-        return Utility.createIconItemStack(Material.EYE_OF_ENDER, 1, ChatColor.YELLOW + "It's a Trap!");
-    }
     public void activate(Gamer gamer, ArenaTeam team) {
-        gamer.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 160, 2));
-        gamer.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 160, 1));
+        gamer.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 100, 1));
+        gamer.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 100, 0));
+    }
+
+    public ItemStack icon() {
+        return Utility.createIconItemStack(Material.FEATHER, 1, ChatColor.YELLOW + "Counter-Offensive Trap");
     }
 }
